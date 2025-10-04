b4fc9f3601fc4f434eef140f3f414b649051e3de
Ahmad Hassan <ahmadhassanyaseen@gmail.com>
2025-10-05 00:16:56 +0500

    New changes

--- app/components/home/Banner3.tsx
+++ app/components/home/Banner3.tsx
@@ -20,7 +20,7 @@ export default function Banner3() {
 
   return (
     <div
-      className="w-full min-h-screen justify-center items-center flex flex-col bg-no-repeat bg-center"
+      className="w-full pt-8 pb-12 justify-center items-center flex flex-col bg-no-repeat bg-center"
       style={{
         backgroundImage: "url('/bg.png')",
         backgroundSize: "cover",
@@ -30,17 +30,17 @@ export default function Banner3() {
       <NavbarBanner/>
 
       {/* Banner Section */}
-      <div className="flex max-w-7xl flex-col md:flex-row justify-between items-center flex-1 px-6 md:px-20 py-10 md:py-0" id="home">
+      <div className="flex mt-3 max-w-7xl flex-col md:flex-row justify-between items-center flex-1 px-6 md:px-20 py-10 md:py-0" id="home">
         {/* Left Content */}
         <div className="max-w-xl text-center md:text-left space-y-5">
           <h2 className="text-3xl md:text-5xl font-bold leading-snug">
-            Talk with our <br />
-            <span className="text-gray-900">Veterinarian approved</span> <br />
-            <span className="text-orange-500">AI pet health expert </span>
-            <span className="text-pink-500">24/7</span>
+            Talk with our 
+            Veterinarian approved 
+            <span className="bg-gradient-to-r from-orange-400 via-pink-500 to-purple-600 bg-clip-text text-transparent"> AI pet health expert 
+            24/7</span>
           </h2>
 
-          <p className="text-gray-700">
+          <p className="text-gray-700 text-xl">
             Available 24/7 on your phone or computer, it helps you check if foods,
             chemicals, or plants are dangerous, guides you step by step during
             emergencies, and pinpoints the closest emergency vet when every
@@ -50,7 +50,7 @@ export default function Banner3() {
           {/* Banner Button */}
          <Link
             href="#"
-            className="bg-orange-500 w-fit hover:bg-orange-600 text-white px-4 py-2 rounded-full text-sm font-semibold flex items-center gap-2 transition"
+            className="bg-orange-500 mx-auto md:mx-0 w-fit hover:bg-orange-600 text-white px-4 py-2 rounded-full text-xl font-semibold flex items-center gap-2 transition"
           >
             Get Started Now
             <span className="relative w-5 h-5 flex items-center justify-center">
